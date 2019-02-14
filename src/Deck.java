a99a484aefd12aa8f0303a0815408621257a686e
rchu6120 <rchu6120@bths.edu>
2019-02-14 14:43:08 -0500

    bdfbdffd

--- src/Deck.java
+++ src/Deck.java
@@ -33,7 +33,8 @@ public class Deck {
      * @param values is an array containing all of the card point values.
      */
     public Deck(String[] ranks, String[] suits, int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        this.cards = new ArrayList<>();
+        for (int i )
     }
 
 
